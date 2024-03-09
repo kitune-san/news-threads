52229f50a818abec77577e9c8c905d31d7929509
kitune-san <19359213+kitune-san@users.noreply.github.com>
2024-03-09 16:07:47 +0900

    Add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default async function Footer() {
+    return (
+        <footer className='justify-center border-b mt-2'>
+            <div className='flex justify-between text-white bg-amber-950'>
+                <p>Footer menu</p>
+            </div>
+            <div className='flex bg-amber-800 text-white'>
+                <p>Footer area</p>
+            </div>            
+        </footer>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/globals.css';
 import Header from '@/app/components/header'
+import Footer from '@/app/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,9 +21,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className='flex flex-col w-full h-full min-h-screen'>
           <Header />
-          <main className='flex flex-col pr-10 pl-10'>
+          <main className='flex flex-col  flex-grow pr-10 pl-10'>
             {children}
           </main>
+          <Footer />
         </div>
       </body>
     </html>
