39b545322bf79da63151682e7b8c5596d27a8c0e
kitune-san <19359213+kitune-san@users.noreply.github.com>
2024-03-17 23:21:21 +0900

    Add post logging

--- app/lib/post.ts
+++ app/lib/post.ts
@@ -7,6 +7,7 @@ import prisma from "@/db";
 import { auth } from "@/auth";
 import { number, z } from 'zod';
 import { Topic, Comment } from '@/app/lib/definitions';
+import { getIP } from '@/app/lib/ip'
 
 const FormSchema = z.object({
     authorId: z.string(),
@@ -30,6 +31,7 @@ export type createTopicState = {
 
 export async function createTopic(prevState: createTopicState, formData: FormData) {
     const session = await auth();
+    const ip = await getIP();
 
     // TODO: validation
     if (session?.user == null) {
@@ -54,20 +56,28 @@ export async function createTopic(prevState: createTopicState, formData: FormDat
     const { authorId, title, body } = validatedPost.data;
  
     try {
-        await prisma.topic.create({
-            data: {
-                authorId: authorId,
-                title: title,
-                body: body
-            },
+        await prisma.$transaction(async (tx) => {
+            const topic = await tx.topic.create({
+                data: {
+                    authorId: authorId,
+                    title: title,
+                    body: body
+                }
+            });
+            await tx.postLog.create({
+                data: {
+                    topicId: topic.id,
+                    authorId: authorId,
+                    title: title,
+                    body: body,
+                    ip: ip
+                }
+            });
         });
     } catch (error) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            return {
-                message: 'Database Error: Failed to Error.'
-            };
-        }
-        throw error;
+        return {
+            message: 'Database Error: Failed to Error.'
+        };
     }
    
     revalidatePath('/');
@@ -89,6 +99,7 @@ export type createCommentState = {
 
 export async function createComment(prevState: createCommentState, formData: FormData) : Promise<createCommentState> {
     const session = await auth();
+    const ip = await getIP();
 
     const commentInfo = {
         topic_id: prevState.topic_id,
@@ -109,6 +120,7 @@ export async function createComment(prevState: createCommentState, formData: For
             message: 'Topic error',
         }
     }
+    const topic_id = commentInfo.topic_id;
     
     const validatedPost = NewPost.safeParse({
         authorId: session.user.id,
@@ -127,23 +139,32 @@ export async function createComment(prevState: createCommentState, formData: For
     const { authorId, title, body } = validatedPost.data;
  
     try {
-        await prisma.comment.create({
-            data: {
-                topicId: commentInfo.topic_id,
-                parentId: commentInfo.parent_id,
-                authorId: authorId,
-                title: title,
-                body: body
-            },
+        await prisma.$transaction(async (tx) => {
+            const comment = await tx.comment.create({
+                data: {
+                    topicId: topic_id,
+                    parentId: commentInfo.parent_id,
+                    authorId: authorId,
+                    title: title,
+                    body: body
+                }
+            });
+            await tx.postLog.create({
+                data: {
+                    topicId: commentInfo.topic_id,
+                    commentId: comment.id,
+                    authorId: authorId,
+                    title: title,
+                    body: body,
+                    ip: ip
+                }
+            });
         });
     } catch (error) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            return {
-                ...commentInfo,
-                message :'Database Error: Failed to comment.'
-            };
-        }
-        throw error;
+        return {
+            ...commentInfo,
+            message :'Database Error: Failed to comment.'
+        };
     }
 
     revalidatePath(`/topic/${commentInfo.topic_id}`);
